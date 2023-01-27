3a77a21bc98d979c7100503c0c092ddea736a69c
so-dami <so94dami@gmail.com>
2023-01-27 16:20:57 +0900

    dfcf

---
+++ 700 .쉐도잉 프로젝트/01.DKB_PJ/02.구현소스/js/common.js
@@ -0,0 +1,15 @@
+// 도깨비 PJ 공통 JS - common.js //
+
+console.log("come on!");
+
+var ham = document.querySelector(".ham");
+console.log("hey, Ham?",ham);
+
+/*********************************************************** 
+    함수명: chgMenu
+    기능: 전체 메뉴 보이기/숨기기
+ **********************************************************/
+function chgMenu(){
+    // 1. 호출 확인
+    console.log("I'm chgMenu");
+} ////////////// chgMenu 함수 //////////////////////////////
