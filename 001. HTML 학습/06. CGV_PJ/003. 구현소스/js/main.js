52b21b7c8e61ae7f2d3672cf3f0219c57cfae3f1
so-dami <so94dami@gmail.com>
2023-02-13 15:18:57 +0900

    ccc

--- 001. HTML 학습/06. CGV_PJ/003. 구현소스/js/main.js
+++ 001. HTML 학습/06. CGV_PJ/003. 구현소스/js/main.js
@@ -40,4 +40,63 @@ window.addEventListener("DOMContentLoaded",()=>{
     }; // click 함수 ///////////////
    } // for of문 //////////////////
 
+   /***************************************
+        [ 극장가는 길 구글맵 보기 기능 ]
+        - 극장가는 길 박스 클릭 시 구글맵 등장
+        - 구글맵 박스의 닫기버튼 클릭 시 구글맵 퇴장
+        -> 방법: css셋팅된 클래스 "on" 넣기/빼기
+   ***************************************/
+    // 1. 대상선정
+        // 1-1. 이벤트 대상: .anibx -> 애니메이션 버튼 박스
+        const anibx = qs(".anibx");
+        // 1-2. 이벤트 대상: .cbtn -> 구글맵 닫기 버튼
+        const cbtn = qs(".cbtn");
+        // 1-3. 변경 대상: .gmap -> 구글맵 박스
+        const gmap = qs(".gmap");
+
+        // console.log(anibx,gmap,cbtn);
+
+    // 2. 버튼에 클릭이벤트 설정하기
+        // 2-1. 극장가는 길 클릭 시 구글맵 박스에 클래스 넣기
+        anibx.onclick = ()=>{
+            gmap.classList.add("on");
+            anibx.classList.add("on");
+        };
+        // 2-2. × 클릭 시 구글맵 박스에 클래스 빼기(구글맵 박스 닫기)
+        cbtn.onclick = ()=>{
+            gmap.classList.remove("on");
+            anibx.classList.remove("on");
+        };
+
+    /***************************************
+        [ SNS 링크 연결하기 ]
+    ***************************************/
+   
+   // 1. 대상 선정: .sns a
+   const sns = qsa(".sns a");
+
+   // 2. 클릭 이벤트 설정
+   // forEach((요소,순번,객체)=>{코드});
+   sns.forEach((ele)=>{
+        ele.onclick=()=>{
+            // 2-1. 내부 텍스트
+            let btxt = ele.innerText;
+            // console.log(btxt);
+
+            // 2-2. 분기문 url 할당하기
+            // 이동주소 변수
+            let url="";
+            switch(btxt){
+                case "페이스북 바로가기": url="http://www.facebook.com/CJCGV"; break;
+                case "트위터 바로가기": url="https://twitter.com/CGV_ID"; break;
+                case "인스타그램 바로가기": url="https://www.instagram.com/cgv_korea/"; break;
+            } ////////// swich case문 //////////
+
+            // 2-3 . 새 창으로 페이지 이동하기
+            // 새창 -> window.open()
+            // 페이지 이동 -> location.href=url값
+            window.open().location.href=url;
+
+        }; ////////// click 함수 //////////
+   }); ////////// forEach //////////
 }); ////////// 로드 구역 //////////
