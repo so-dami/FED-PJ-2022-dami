3d65fbdc14458924a300417c88b119ac6f61dcdb
so-dami <so94dami@gmail.com>
2023-02-23 15:59:26 +0900

    0

--- 700. 쉐도잉 프로젝트/02. VOGUE_PJ/002.site/js/main.js
+++ 700. 쉐도잉 프로젝트/02. VOGUE_PJ/002.site/js/main.js
@@ -41,11 +41,45 @@ window.addEventListener("DOMContentLoaded",()=>{
     // 1. 대상: .scAct
     const scAct = q(".scAct");
 
-    // 2. 스크롤 이벤트 셋팅
+    // 2. 화면높이값의 2/3구하기
+    const hv = window.innerHeight/3*2;
+    // console.log("2/3높이:",hv);
+
+    // 3. 클래스 넣기 함수 만들기
+    const showIt = x => { // x - 등장요소
+        // 대상요소의 현재스크롤 위치
+        let xval = retVal(x);
+
+        // 구간적용여부 검사하기
+        // 0보다 크고 화면의 2/3보다 작은 구간!
+        if(xval < hv && xval > 0){
+            // console.log("작동");
+            // 해당요소에 클래스 넣기!
+            x.classList.add("on");
+        } // if
+        
+        // 되돌리기(on 제거)는 else문에 구현 가능
+        // else{
+        //     x.classList.remove("on");
+        // } // else
+
+    }; // showIt
+
+    // 4. 스크롤 이벤트 셋팅
     window.addEventListener("scroll",()=>{
 
         // 2-1. 값 확인
-        cg("등장 박스1: "+retVal(scAct[0]));
+        // cg("등장 박스1: "+retVal(scAct[0]));
+
+        // 2-2. 스크롤등장 요소 개수만큼 for문으로 돌리기
+        for(let x of scAct) showIt(x);
+        // 순서:
+        // 1. 스크롤 이벤트시 setit에 담긴 요소를 수집
+        // 2. 요소만큼 x에 하나씩 순서대로 담는다
+        // 3. showIt(x) 호출시 x, 즉 요소자신을 보낸다!
+        // 4. showIt() 함수는 그 x의 위치값을 조사해서
+        // if문 구간에 해당하는지 검사후 해당요소에
+        // 클래스 "on"을 넣어준다!
         
     }); // scroll
 
