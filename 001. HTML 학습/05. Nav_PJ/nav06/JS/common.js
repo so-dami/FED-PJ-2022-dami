a2bb99b1238f3f6baf1f435ffde25c4ae10887f6
so-dami <so94dami@gmail.com>
2023-02-28 14:01:22 +0900

    0

---
+++ 001. HTML 학습/05. Nav_PJ/nav06/JS/common.js
@@ -0,0 +1,12 @@
+// 네이게이션 유형6: 공통 js - common.js
+
+// 로드 구역
+window.addEventListener("DOMContentLoaded",loadFn);
+
+/* 
+    함수명: loadFn
+    기능: 로딩 후 실행함수
+*/
+function loadFn(){
+    
+}; // loadFn 함수 //
