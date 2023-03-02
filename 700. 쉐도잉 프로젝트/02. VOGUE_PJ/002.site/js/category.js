6a377fe1ea3fbd95f2908c0f485b83984dcf47d5
so-dami <so94dami@gmail.com>
2023-03-02 12:20:09 +0900

    d

---
+++ 700. 쉐도잉 프로젝트/02. VOGUE_PJ/002.site/js/category.js
@@ -0,0 +1,9 @@
+// 보그 PJ 카테고리 페이지 js - category.js
+
+// 로딩 구역 //
+window.addEventListener("DOMContentLoaded",loadFn);
+
+// 로드 함수 //
+function loadFn(){
+    console.log("category");
+} // loadFn 함수 //
---
+++ 700. 쉐도잉 프로젝트/02. VOGUE_PJ/002.site/js/linksys.js
@@ -0,0 +1,9 @@
+// 보그 PJ 링크시스템 페이지 js - linksys.js
+
+// 로딩 구역 //
+window.addEventListener("DOMContentLoaded",linkFn);
+
+// 로드 함수 //
+function linkFn(){
+    console.log("linksys");
+} // linkFn 함수 //
