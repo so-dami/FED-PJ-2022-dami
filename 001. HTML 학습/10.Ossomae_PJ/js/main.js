cbd0335ae467949d85cc87d20842e029248ee33a
so-dami <so94dami@gmail.com>
2023-03-14 10:20:59 +0900

    Update main.js

--- 001. HTML 학습/10.Ossomae_PJ/js/main.js
+++ 001. HTML 학습/10.Ossomae_PJ/js/main.js
@@ -5,6 +5,36 @@ window.addEventListener("DOMContentLoaded",()=>{
 
     console.log("로딩완료!");
 
+    // 대상: .gbx
+    const gbx = document.querySelector(".gbx");
+
+    document.querySelector(".rb")
+    .onclick = () => {
+        console.log("오른쪽");
+
+        // 이동 대상: .gbx>div
+        let tg = gbx.querySelectorAll("div");
+
+        // 첫 번째 자식요소 div 맨 뒤로 이동
+        // appendChild(첫번째요소);
+        gbx.appendChild(tg[0]);
+        
+    }; // click //
+
+    document.querySelector(".lb")
+    .onclick = () => {
+        console.log("왼쪽");
+
+        // 이동 대상: .gbx>div
+        let tg = gbx.querySelectorAll("div");
+
+        // 마지막 자식요소 div 맨 앞으로 이동
+        // insertBefore(마지막요소,첫번째요소);
+        gbx.insertBefore(tg[tg.length-1],tg[0]);
+        // 마지막 요소: 개수-1
+        
+    }; // click //
+
     
 }); ///////////// 로딩구역 //////////////////////
 /////////////////////////////////////////////////
