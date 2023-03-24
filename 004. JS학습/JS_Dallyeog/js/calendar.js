8d3049ccccc6bdf179504b3ffa2154a9025ffac4
so-dami <so94dami@gmail.com>
2023-03-24 12:44:49 +0900

    xzd

--- 004. JS학습/JS_Dallyeog/js/calendar.js
+++ 004. JS학습/JS_Dallyeog/js/calendar.js
@@ -1,5 +1,8 @@
 // 달력 생성자함수 //
 
+// 호출
+MakeDallyeok();
+
 function MakeDallyeok(){
 
     // 선택함수
@@ -10,6 +13,7 @@ function MakeDallyeok(){
     const cg = x => console.log(x);
     
     // 1. 변수 셋팅
+
         // (1) 변경할 현재 날짜 객체
         const curr_date = new Date();
 
@@ -24,5 +28,24 @@ function MakeDallyeok(){
 
         // (5) 날짜 요소: .dates
         const dates = qs(".dates");
-        
+
+        cg(dates);
+
+    // 2. 함수 생성
+
+        // (1) 달력 초기화 구성 함수
+        const initDallyeok = () => {
+
+            // getMonth() 정보는 항상 현재달 숫자보다 -1 (배열순번)
+
+            // (1-1) 전달 마지막 날짜(옵션: 0)
+            // -> 달력 전달 끝쪽 날짜 표시
+            const prevLast = new Date(curr_date.getFullYear(),curr_date.getMonth(),0)
+            cg(prevLast);
+
+        }; // initDallyeok //
+
+        // 함수 호출
+        initDallyeok();
+
 } // MakeDallyeok //
