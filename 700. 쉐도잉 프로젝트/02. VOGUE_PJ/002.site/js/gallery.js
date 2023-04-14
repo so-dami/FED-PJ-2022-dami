97a55fe05be915c89b86fc7a6352abf1ca789cf0
so-dami <so94dami@gmail.com>
2023-04-14 11:01:24 +0900

    Update gallery.js

--- 700. 쉐도잉 프로젝트/02. VOGUE_PJ/002.site/js/gallery.js
+++ 700. 쉐도잉 프로젝트/02. VOGUE_PJ/002.site/js/gallery.js
@@ -44,6 +44,33 @@ $(() => {
             // false - 드래그 하려고 슬라이드 건들여도 다시 안 건들면 일정시간 후에 자동넘김 재시동
             disableOnInteraction: false,
         },
+
+        // 사이즈별 슬라이드 개수, 간격 동적 변경 셋팅하기(미디어쿼리)
+        // Responsive breakpoints
+        breakpoints: {
+
+            // when window width is >= 200px
+            200: {
+            slidesPerView: 1,
+            spaceBetween: 0
+            },
+
+            // when window width is >= 700px
+            700: {
+            slidesPerView: 2,
+            spaceBetween: 10
+            },
+
+            // when window width is >= 1000px
+            1000: {
+            slidesPerView: 3,
+            spaceBetween: 20
+            }
+        }
     });
+
+    // 테스트: 스와이퍼의 인스턴스를 생성 후 특정기능의 속성과 메서드를 사용할 수 있음
+    // ex) Gallery라는 제목을 클릭하면 다음 슬라이드 보이기 기능 구현
+    $(".stit").click(()=>swiper.slideNext()); // click //
 
 }); // jQB //
