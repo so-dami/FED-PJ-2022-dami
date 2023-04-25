3ccbfd7f6afae010aede39fa3d6c84b80a85857d
so-dami <so94dami@gmail.com>
2023-04-25 16:20:39 +0900

    zdf

--- 700. 쉐도잉 프로젝트/03.Pilot_PJ/002.구현소스/js/jquery-autoScroll.js
+++ 700. 쉐도잉 프로젝트/03.Pilot_PJ/002.구현소스/js/jquery-autoScroll.js
@@ -1,200 +1,198 @@
 // 제이쿼리로 구현한 자동페이지 휠 JS : jquery-autoScroll.js
-// 로딩구역 없이 함수로 구현함
 
-// 전체를 함수로 묶기
-// 이유? export 하기 위해
+// 로딩구역없이 함수로 구현함! /////
 
-function autoScroll(){
+// 전체를 함수로 묶어준다! ///////
+// 이유: export 하기위해!
 
-/* 대상 변수 할당하기 */
+function autoScroll() {
+    /****************************************** 
+        대상 변수할당하기
+    ******************************************/
     // 전체 페이지번호
-    let pno = 0
-
+    let pno = 0;
     // 페이지 요소
     const pg = $(".page");
-
-    // 전체 페이지 갯수
+    // 전체 페이지개수
     const pgcnt = pg.length;
-    console.log("페이지수:",pgcnt,pg);
-
-    // 광실행 금지 변수
+    // console.log("페이지개수:", pgcnt, pg);
+    // 광실행금지변수
     let prot = [];
-
-    // 광스크롤 금지 변수
+    // 광스크롤금지
     prot[0] = 0;
-
-    // gnb 메뉴 li
+    // GNB 메뉴 li
     const gnb = $(".gnb li");
-    
-    // 인디케이터 메뉴 li
+    // indic 메뉴 li
     const indic = $(".indic li");
-
-    // 각 페이지별 등장 요소
+    // 각 페이지별 등장요소
     const minfo = $(".minfo");
 
-/* 이벤트 등록하기 */
-    // 윈도우 휠이벤트 발생 시
-    $(window).on("wheel",wheelFn);
-
-    // gnb 메뉴 클릭 시
-    // 대상: .gnb a
+    /****************************************** 
+        이벤트 등록하기
+    ******************************************/
+    // 윈도우 휠이벤트 발생시
+    $(window).on("wheel", wheelFn);
+    // GNB메뉴 클릭시 : 대상 - .gnb a
     $(".gnb a").click(chgMenu);
-
-    // 인디케이터 클릭 시
-    // 대상: .indic a
+    // 인디케이터 클릭시 : 대상 - .indic a
     $(".indic a").click(chgMenu);
 
-    // 새로고침 시 스크롤 위치 캐싱 변경하기(맨 위로)
-    $("html,body").animate({scrollTop: "0px"});
+    // 새로고침시 스크롤위치 캐싱 변경하기(맨위로!)
+    $("html,body").animate({ scrollTop: "0px" });
 
-/****************************************
+    /**************************************** 
+        함수명: wheelFn
+        기능: 마우스휠 이벤트 발생시 호출됨
+        -> 한페이지씩 자동스크롤 기능
+    ****************************************/
+    function wheelFn() {
+        // 광휠금지
+        if (prot[0]) return;
+        chkCrazy(0);
 
-    [ 스크롤 적용 함수 ]
-    
-    함수명: wheelFn
-    기능: 마우스휠 이벤트 발생 시 호출됨
-    -> 한 페이지씩 자동스크롤 기능
+        console.log("휠~~~~~~!");
 
-****************************************/
-function wheelFn(){
+        // 1.휠방향 알아내기
+        let delta = event.wheelDelta;
+        console.log(delta);
 
-    // 광휠 금지
-    if(prot[0]) return;
-    chkCrazy(0);
-    
-    console.log("휠");
+        // 2. 방향에 따른 페이지번호 증감
+        if (delta < 0) {
+            pno++;
+            if (pno === pgcnt) pno = pgcnt - 1;
+            // 마지막 페이지번호에 고정!
+        } //// if /////
+        else {
+            pno--;
+            if (pno === -1) pno = 0;
+            // 첫페이지번호에 고정!
+        } //// else ////
 
-    // 1. 휠 방향 알아내기
-    let delta = event.wheelDelta;
-    console.log(delta);
+        console.log(pno);
 
-    // 2. 방향에 따른 페이지 번호 증감
-    if(delta < 0){
-        pno++;
-        
-        // 마지막 페이지 번호에 고정
-        if(pno===pgcnt)pno=pgcnt-1;
+        // 3. 스크롤 이동하기 + 메뉴에 클래스"on"넣기
+        movePg();
+    } /////////////// wheelFn 함수 ///////////////
 
-    } // if //
+    // 광클 초기값
+    prot[1] = 0;
+    /******************************************** 
+        함수명: chgMenu
+        기능: 메뉴 클릭시 메뉴변경과 페이지이동
+    ********************************************/
+    function chgMenu() {
+        // 0. 광클금지
+        if (prot[1]) return;
+        chkCrazy(1);
 
-    else{
-        pno--;
+        // 1. 클릭된 a요소의 부모 li 순번을 구함 === pno
+        let idx = $(this).parent().index();
 
-        // 첫 페이지 번호에 고정
-        if(pno===-1)pno=0;
-        
-    } // else //
+        console.log("나,클릭?", this, idx);
 
-    console.log(pno);
+        // 2. 전역페이지번호에 순번 업데이트
+        pno = idx;
 
-    // 3. 스크롤 이동하기 + 메뉴에 클래스 "on" 넣기
-    movePg();
-    
-}; // wheelFn //
+        // 3. 페이지이동 + 메뉴에 클래스"on"넣기
+        movePg();
+    } ////////// chgMenu 함수 ///////////////////
 
-/*********************************************
-    
-    함수명: chgMenu
-    기능: 메뉴 클릭 시 메뉴 변경과 페이지 이동
+    /******************************************** 
+        함수명: chkCrazy
+        기능: 광적동작 체크하여 제어리턴
+    ********************************************/
+    function chkCrazy(seq) {
+        // seq 관리변수 순번
+        prot[seq] = 1;
+        setTimeout(() => (prot[seq] = 0), 800);
+    } //////// chkCrazy함수 //////////////
 
-*********************************************/
+    /******************************************** 
+        함수명: movePg
+        기능: 페이지이동 애니메이션
+    ********************************************/
+    function movePg() {
+        // 대상: html,body -> 두개를 모두 잡아야 공통적으로 적용됨!
+        $("html,body").animate(
+            {
+                scrollTop: $(window).height() * pno + "px",
+            },
+            700,
+            "easeInOutQuint",
+            actPage // 이동후 콜백함수호출!
+        );
 
-// 광클 초기값
-prot[1] = 0;
+        // 대상: GNB메뉴 , 인디케이터 메뉴
+        gnb.eq(pno).addClass("on").siblings().removeClass("on");
+        indic.eq(pno).addClass("on").siblings().removeClass("on");
+    } ///////////////// movePg ////////////////
 
-function chgMenu(){
+    /******************************************** 
+        [ 페이지 등장액션 요소 적용하기 ]
+        1. 이벤트 적용시점 : 페이지도착후(애니후콜백) 
+        2. 이벤트 대상 : 각 페이지 동일
+            (1) .page .imgc - 이미지파트
+            (2) .page .txtc h2 a - 타이틀파트
+        3. 변경내용 :
+            [스타일시트 아래 항목 변경]
+            ((변경값))
+            transform: rotate(45deg);
+            opacity: 0;
+            transition: 1s 1s; -> 타이틀만 지연시간
+            ((고정값))
+            transform-origin: left top;
+            display: inline-block; -> a요소만
+    ********************************************/
 
-    // 0. 광클 금지
-    if(prot[1]) return;
-    chkCrazy(1);
-    
-    // 1. 클릭된 a 요소의 부모인 li 순번 구하기 === pno
-    let idx = $(this).parent().index();
-    console.log("click",this,idx);
+    /******************************************* 
+        함수명: initSet
+        기능: 등장요소 처음상태 셋팅
+    *******************************************/
+    function initSet() {
+        // 1. 초기화하기 ///////////
+        // 대상: .imgc
+        $(".imgc").css({
+            transform: "rotate(45deg)",
+            transformOrigin: "-10% -10%",
+            opacity: "0",
+            transition: "1s ease-in-out",
+        }); /////////// css ///////////
 
-    // 2. 전역 페이지 번호에 순번 업데이트
-    pno = idx;
-    
-    // 3. 페이지 이동
-    movePg();
+        // 대상: .txtc a
+        $(".txtc a").css({
+            transform: "rotate(45deg)",
+            transformOrigin: "-100% -100%",
+            opacity: "0",
+            transition: "1s ease-in-out .5s",
+            display: "inline-block",
+        }); /////////// css ///////////
+    } ////////////// initSet 함수 //////////////
 
-    // 4. 메뉴에 클래스 "on" 넣기
-    // 대상: gnb 메뉴, 인디케이터 메뉴 -> gnb, indic 변수
-    gnb.eq(pno).addClass("on").siblings().removeClass("on");
-    indic.eq(pno).addClass("on").siblings().removeClass("on");
-    
-    
-}; // chgMenu //
+    // 초기화함수 호출
+    initSet();
 
-/***************************************
-    
-    함수명: chkCrazy
-    기능: 광적 동작을 체크하여 제어 리턴
+    /**************************************** 
+        함수명: actPage
+        기능 : 페이지 도착후 등장애니메이션
+    ****************************************/
+    function actPage() {
+        // 이동후 확인
+        console.log("액숀~!!", pno);
 
-***************************************/
-function chkCrazy(seq){ // seq - 관리 변수 순번
+        // pno가 0 또는 4가 아니면 액션작동!
+        if (pno !== 0 || pno !== 4) {
+            // 대상: 해당순번의 .page 아래 .imgc 와 .txtc a
+            $(`.page:eq(${pno}) .imgc,
+            .page:eq(${pno}) .txtc a`).css({
+                transform: "rotate(0deg)",
+                opacity: "1",
+            }); /////////// css ///////////
+        } ////////// if ///////////////////
 
-    prot[seq] = 1;
-    setTimeout(()=>prot[seq]=0,800);
-    
-}; // chkCrazy //
+        // 첫페이지 일때 초기화!
+        if (pno === 0) initSet();
+    } ///////////// actPage 함수 ////////////
+} //////////// autoScroll /////////////////
 
-/***************************************
-    
-    함수명: movePg
-    기능: 페이지 이동 애니메이션
-
-***************************************/
-function movePg(){
-
-    // 대상: html,body -> 두 개를 모두 잡아야 공통적으로 적용됨
-    $("html,body")
-    .animate({
-        scrollTop: ($(window).height()*pno)+"px"
-    },800,"easeOutBounce",showEle); // 이동 후 콜백함수 호출 
-
-    // 대상: gnb 메뉴, 인디케이터 메뉴 -> gnb, indic 변수
-    gnb.eq(pno).addClass("on").siblings().removeClass("on");
-    indic.eq(pno).addClass("on").siblings().removeClass("on");
-    
-}; // movePg //
-
-/***************************************
-    
-    함수명: showEle
-    기능: 페이지 이동 후 요소 등장하기
-
-***************************************/
-
-// 등장할 요소 초기화
-minfo.css({
-    opacity: 0,
-    transform: "translate(-50%,50%)",
-    transition: ".3s ease-out"
-}); // css //
-
-function showEle(){
-
-    // .minfo 페이지별 등장하기
-    pg.eq(pno).find(".minfo")
-    .css({
-        opacity: 1,
-        transform: "translate(-50%,-50%)",
-    }) // css //
-    // 다른 페이지 초기화
-    .parents(".page").siblings().find(".minfo")
-    .css({
-        opacity: 0,
-        transform: "translate(-50%,50%)",
-        transition: ".3s ease-out"
-    }); // css //
-    
-}; // showEle //
-
-// 등장 액션 함수 최초 호출
-setTimeout(showEle, 1000);
-
-} // autoScroll //
-
-// 전체 함수 내보내기 //
+// 전체함수 내보내기 ///////
 export default autoScroll;
