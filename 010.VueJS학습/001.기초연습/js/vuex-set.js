bb8798177d7847d9fab419f0f81758a945977660
so-dami <so94dami@gmail.com>
2023-05-03 16:21:03 +0900

    zdff

---
+++ 010.VueJS학습/001.기초연습/js/vuex-set.js
@@ -0,0 +1,34 @@
+// 뷰x 스토어 구현 js - vuex-set.js
+
+// 1. 상단영역 컴포넌트 셋팅
+Vue.component("top-area",{
+    template: ``,
+    data(){
+        return{}
+    },
+    methods:{
+
+    }
+}) // top-area Vue //
+
+// 2. 메인영역 컴포넌트 셋팅
+Vue.component("main-area",{
+    template: ``,
+    data(){
+        return{}
+    },
+    methods:{
+
+    }
+}) // main-area Vue //
+
+// 3. 하단영역 컴포넌트 셋팅
+Vue.component("info-area",{
+    template: ``,
+    data(){
+        return{}
+    },
+    methods:{
+
+    }
+}) // info-area Vue //
