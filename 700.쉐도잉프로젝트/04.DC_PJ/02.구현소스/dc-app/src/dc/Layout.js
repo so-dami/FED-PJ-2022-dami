6c316f4ec58e8a8e3f115f354ab97c461a2eca4d
so-dami <so94dami@gmail.com>
2023-06-09 10:51:08 +0900

    df

--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Layout.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Layout.js
@@ -18,7 +18,70 @@ import { Link, Outlet } from "react-router-dom";
     -> 라우터 연결 컴포넌트 출력자리
 ********************************************************/
 
+// 객체: 데이터 내용을 구체적으로 구분하여 반복
+// 배열: 데이터 단순 반복
+
 const Layout = () => {
+
+    // gnb 메뉴 데이터 구성 - 배열 데이터
+    const gnb_data = [
+        {
+            txt: "HOME",
+            link: "/",
+        },
+        {
+            txt: "CHARACTERS",
+            link: "/ct",
+        },
+        {
+            txt: "COMICS",
+            link: "/co",
+            sub: [
+                {
+                    txt: "LATEST COMICS",
+                    link: "/lc"
+                },
+                {
+                    txt: "DC UNIVERSE INFINITE",
+                    link: "/dui"
+                },
+                {
+                    txt: "ALL COMICS SERIES",
+                    link: "/acs"
+                },
+            ]
+        },
+        {
+            txt: "MOVIES & TV",
+            link: "/mv",
+            sub: [
+                {
+                    txt: "DC MOVIE",
+                    link: "/dm"
+                },
+                {
+                    txt: "DC SERIES",
+                    link: "/ds"
+                },
+                {
+                    txt: "DC ON HBO MAX",
+                    link: "/hbo"
+                },
+            ]
+        },
+        {
+            txt: "GAMES",
+            link: "/gm",
+        },
+        {
+            txt: "NEWS",
+            link: "/nw",
+        },
+        {
+            txt: "VIDEO",
+            link: "/vd",
+        },
+    ];
 
     return(
 
@@ -37,27 +100,39 @@ const Layout = () => {
                         </li>
 
                         {/* gnb 메뉴 */}
-                        <li>
-                            <Link to="/">HOME</Link>
-                        </li>
-                        <li>
-                            <Link to="/ct">CHARACTERS</Link>
-                        </li>
-                        <li>
-                            <Link to="/co">COMICS</Link>
-                        </li>
-                        <li>
-                            <Link to="/mv">MOVIES & TV</Link>
-                        </li>
-                        <li>
-                            <Link to="/gm">GAMES</Link>
-                        </li>
-                        <li>
-                            <Link to="/nw">NEWS</Link>
-                        </li>
-                        <li>
-                            <Link to="/vd">VIDEO</Link>
-                        </li>
+                        {
+                            gnb_data.map((v,i)=>
+                                <li key={i}>
+                                    <Link to={v.link}>{v.txt}</Link>
+                                    {/* {console.log(v.sub)} */}
+                                    
+                                    {/* v.sub가 없으면 undefined */}
+                                    {
+                                        // 조건식 && 출력코드
+                                        // 조건: sub 데이터가 없지 않으면
+
+                                        // undefined: 정의되지 않은 값
+                                        // null: 빈 값
+                                        // -> 위 두가지는 데이터가 없다는 하나의 값
+
+                                        v.sub != undefined &&
+                                        <div className="smenu">
+                                            <ol>
+                                                {
+                                                    v.sub.map((v,i)=>
+                                                        <li key={i}>
+                                                            <Link to={v.link}>{v.txt}</Link>
+                                                        </li>
+                                                    )
+                                                }
+                                            </ol>
+                                        </div>
+                                    }
+                                </li>
+                            )
+                        }
+
+                        
 
                     </ul>
                 </nav>
