4ca3736aefbc1100095e03a4242d226326557317
so-dami <so94dami@gmail.com>
2023-06-16 12:55:48 +0900

    zdf

---
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/data/cat.js
@@ -0,0 +1,13 @@
+// 캐릭터 컴포넌트 사용 데이터 - cat.js
+
+const cat_data = [
+    {
+        idx: "",
+        tmsrc: "",
+        cname: "",
+        cdesc: "",
+        facts: "",
+        alignment: "",
+        type: ""
+    },
+];
