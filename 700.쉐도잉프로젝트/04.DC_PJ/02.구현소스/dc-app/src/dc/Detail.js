281c19f362ce3834d11a3f4f650c47963bfe55d7
so-dami <so94dami@gmail.com>
2023-06-19 12:44:39 +0900

    zdfzdf

--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Detail.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Detail.js
@@ -1,13 +1,39 @@
 // 캐릭터 상세 페이지 컴포넌트 - Detail.js
 // 라우터 파라미터값 받아서 데이터 처리하기
 
+import { useLocation } from "react-router-dom";
+
 function Detail(props){
+    
+    // 라우터 전달값을 받기 위한 useLocation 생성하기
+    const loc = useLocation();
+
+    // 보낸 속성명을 변수에 할당하기
+    // state.속성명: 내가 라우터를 통해 보낸 속성명 받기
+
+        // 1. 캐릭터 이름
+        const cname = loc.state.cname;
+        // 2. 캐릭터 설명 - '^' 문자로 잘라 배열로 데이터 변경
+        let cdesc = loc.state.cdesc;
+        cdesc = cdesc.split('^');
+        // 3. 캐릭터 명세
+        const facts = loc.state.facts;
 
     return(
 
         <>
 
-            <h2>나는 상세 페이지야~</h2>
+            <h2>{cname}</h2>
+            <div className="cdesc">
+                {
+                    cdesc.map(v=><p>{v}</p>)
+                }
+            </div>
+            
+            <div className="facts">
+                <h3>CHARACTER FACTS</h3>
+                {facts}
+            </div>
             
         </>
         
--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/plugin/SwiperCat.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/plugin/SwiperCat.js
@@ -55,13 +55,30 @@ export default function SwiperCat(props){
                     
                     <SwiperSlide key={i}>
 
-                        <Link to="/det">
+                        {/* /det 라우터 컴포넌트 페이지 호출 시 state 속성값으로 객체를 보내어 값을 전달함 */}
+
+                        {/* 도착 페이지인 Detail.js 컴포넌트에 페이지를 나타내야 할 데이터 항목을 데이터 속성명과 같은 이름으로 셋팅하여 라우터 전달 state객체에 담아서 보냄*/}
+
+                        {/* selcat은 캐릭터 이름 전달 속성명 */}
+                        {/* 값은 v.cname으로 캐릭터명을 보내줌 */}
+
+                        {/*
+                            cname 캐릭터 이름
+                            cdesc 캐릭터 설명
+                            facts 캐릭터 명세
+                        */}
+                        
+                        <Link to="/det"
+                        state={{
+                            cname:v.cname,
+                            cdesc:v.cdesc,
+                            facts:v.facts
+                        }}>
                             <section className="swinbx">
 
                                 {/* 캐릭터 이미지 영역 */}
                                 <div className="catimg">
                                     <img src={v.tmsrc} alt={v.cname} />
-                                    
                                 </div>
                                 
                                 {/* 캐리겉 타이틀 영역 */}
