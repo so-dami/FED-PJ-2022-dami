3af5c3d3cb5a47c867fc9df64faa7edf776c0ec2
so-dami <so94dami@gmail.com>
2023-07-03 11:30:32 +0900

    ㅇㄹㅋㅇ

--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Layout.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Layout.js
@@ -1,161 +1,137 @@
 // 메인 레이아웃 컴포넌트
-
-// 로고
 import Logo from "./Logo";
-
-// 레이아웃 CSS
 import "./css/layout.css";
-
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import { gnb_data, bmenu } from "./data/common";
+import $ from 'jquery';
 
-// 폰트어썸
+/* 폰트어썸 임포트 */
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { useState } from "react";
 import ScrollTop from "./common/ScrollTop";
-
-/********************************************************
+/******************************************************* 
     [ 리액트 라우터와 연결하여 사용되는 라우터 컴포넌트 ]
-    
     1. <Link to="/경로명"></Link>
-    -> to 속성의 경로는 <Route Path="/경로명">와 일치함
+    -> to속성의 경로는 <Route path="/경로명"> 과 일치함!
 
     2. <Outlet />
-    -> 라우터 연결 컴포넌트 출력자리
-********************************************************/
-
-// 객체: 데이터 내용을 구체적으로 구분하여 반복
-// 배열: 데이터 단순 반복
+    -> 라우터 연결 컴포넌트 출력자리 컴포넌트
+*******************************************************/
 
 const Layout = () => {
 
     // 라우터 이동메서드 셋팅!
-    const goNav = useNavigate();
+    const goNav = useNavigate();   
 
-    // 자식 컴포넌트 값 전달 테스트
+
+    // 자식컴포넌트 값 전달 테스트 함수
     const callMe = (x) => {
+        console.log("누구?", x);
+    }; ////////// callMe /////////////
 
-        console.log("누구?",x);
-        
-    }; // callMe //
-
-    // 로그인 상태 Hook 변수: 로컬스 "minfo" 할당
-    const [logSts,setLogSts] = useState(localStorage.getItem("minfo"));
-
-    // 로그인 환영 메시지 Hook 변수
-    const [logMsg,setLogMsg] = useState("");
-
-    // 로그인 환영 메시지 스타일
+    // 로그인 상태  Hook변수 : 로컬쓰 "minfo" 초기할당!
+    const [logSts, setLogSts] = useState(localStorage.getItem("minfo"));
+    // 로그인 환영메시지 Hook변수
+    const [logMsg, setLogMsg] = useState("");
+    // 로그인 환영메시지 스타일
     const logstyle = {
         position: "absolute",
         left: "50%",
-        transform: "translateX(-50%)"
-    }; // logstyle //
+        transform: "translateX(-50%)",
+    }; //////// logstyle ///////////
 
-    // 로그인 셋팅 함수
-    // -> ScrollTop.js의 useEffect 함수 구역에서 호출
+    // 로그인 셋팅 함수 //////////
+    // -> ScrollTop.js 의 useEffect 함수구역에서 호출!
     const setLogin = () => {
-
-        // 1. 로그인 Hook 변수 업데이트
+        // 1. 로그인 Hook변수 업데이트하기
         setLogSts(localStorage.getItem("minfo"));
 
-        // 2. 로컬스 값이 null이 아니면 메시지 뿌리기
-        if(localStorage.getItem("minfo")){
+        // 2. 로컬쓰값이 null이 아니면 메시지 뿌리기
+        if (localStorage.getItem("minfo")) {
+            // 메시지 셋팅하기 : 객체안의 "unm"속성이 사용자이름!
+            setLogMsg("Welcome " + JSON.parse(localStorage.getItem("minfo"))["unm"]);
+        } ////////////// if ///////////////
+    }; ///////// setLogin ////////////
 
-            // 메시지 셋팅하기: 객체 안의 "unm" 속성이 사용자 이름
-            setLogMsg("Welcome " + 
-            JSON.parse(localStorage.getItem("minfo"))["unm"]);
-            
-        } // if //
-        
-    }; // setLogin //
-
-    // 로그아웃 함수
-    // -> LOGOUT 버튼에서 호출
+    // 로그아웃 함수 ///////////////////
+    // -> LOGOUT 버튼에서 호출함!
     const logout = () => {
-
-        // 1. 로컬스 "minfo" 삭제하기
+        // 1. 로컬쓰 "minfo" 삭제하기
         localStorage.removeItem("minfo");
-
-        // 2. 로그인 상태 Hook 변수 업데이트 하기
+        // 2. 로그인상태 Hook 변수 업데이트하기
         setLogSts(null);
-        console.log("로그아웃",logSts);
-        
-    }; // logout //
+        console.log("로그아웃됨!", logSts);
+    }; ////////////// logout ///////////
 
-     // 검색창 보이기 함수
-     const showSearch = () => {
-        // 1. a 요소 숨기기
+    // 검색창 보이기 함수 ///////
+    const showSearch = () => {
+        // 1. a요소숨기기
         document.querySelector(".searchingGnb+a")
         .style.opacity = "0";
 
         // 2. 검색창 보이기
-        let tg = document.querySelector(".searchingGnb");
+        let tg =
+        document.querySelector(".searchingGnb");
         tg.style.display = "block";
         tg.querySelector("input").focus();
 
-    }; // showSearch //
+    }; ////////// showSearch /////////////
 
-    // 입력창에서 엔터키를 누르면 검색 함수 호출
+    // 입력창에서 엔터키를 누르면 검색함수 호출!
     const enterKey = (e) => {
         if (e.key === "Enter") goSearch();
-    }; // enterKey 함수 //
+    }; //////////// enterKey 함수 ////////////
 
-    // 검색 페이지로 검색어와 함께 이동하기
+    // 검색페이지로 검색어와 함께 이동하기 //////
     const goSearch = () => {
-
         // 검색어 읽어오기
         let kw = document.querySelector(".searchingGnb input").value;
         console.log("검색어:",kw);
-
         // 라우터 이동하기 : 전달값 가져가기(검색어)
         goNav('/res',{state:{keyword:kw}})
+    }; ///////////// goSearch ////////////////
 
-    }; // goSearch //
+    ///////// 상단메뉴 변경 함수 ///////////////
+    // .top.on 이면 메뉴가 나타남!
+    const chgMenu = () => $(".top").toggleClass('on');
 
-    return(
 
+
+    return (
         <>
-            {/* 라우터 갱신될때 스크롤 상단이동 모듈 작동 + 로그인 셋팅 함수 호출 전달하기, 자식에게 setLogin 함수 전달 */}
-			<ScrollTop sfn={setLogin} />
-        
-            {/* 1. 상단 영역 */}
+            {/* 라우터 갱신될때 스크롤 상단이동 모듈작동함!
+            + 로그인셋팅함수 호출전달하기! 자식에게 setLogin함수전달 */}
+            <ScrollTop sfn={setLogin} />
+            {/* 1.상단영역 */}
             <header className="top">
-
-                {/* 로그인 환영 메시지 - 조건: logSts 값이 null이 아니면 */}
-                {
-                    logSts !== null &&
+                {/* 로그인 환영메시지 : 조건-logSts값이 null이 아니면 */}
+                {logSts !== null && (
                     <div className="logmsg" style={logstyle}>
                         {logMsg}
                     </div>
-                }
-                
+                )}
 
-                {/* 네비게이션 */}
+                {/* 네비게이션 파트 */}
                 <nav className="gnb">
                     <ul>
-
-                        {/* 로고 */}
                         <li>
                             <Link to="/main">
                                 <Logo gb="top" tt={callMe} />
                             </Link>
                         </li>
-
-                        {/* gnb 메뉴 */}
                         {gnb_data.map((v, i) => (
-                            <li key={i}>
+                            <li key={i} onClick={chgMenu}>
                                 <Link to={v.link}>{v.txt}</Link>
                                 {/* {console.log(v.sub)} */}
                                 {/* v.sub가 없으면 undefined */}
                                 {
                                     // 조건식 && 출력코드
-                                    // 조건: sub 데이터가 없지 않으면
-                                    // undefined - 정의되지 않은 값
-                                    // null - 빈 값
-                                    // 위의 두 가지는 데이터가 없다는 값임
+                                    // 조건: sub데이터가 없지 않으면
+                                    // undefined - 정의되지 않은값
+                                    // null - 빈값
+                                    // 위의 두가지는 데이터가 없다는 값임!
                                     v.sub != undefined && (
                                         <div className="smenu">
                                             <ol>
@@ -169,21 +145,18 @@ const Layout = () => {
                                     )
                                 }
                             </li>
-                        ))} 
+                        ))}
 
                         <li style={{ marginLeft: "auto" }}>
-
                             {/* 검색입력박스 */}
                             <div className="searchingGnb">
-
-                                {/* 검색 버튼 돋보기 아이콘 */}
+                                {/* 검색버튼 돋보기아이콘 */}
                                 <FontAwesomeIcon
                                     icon={faSearch}
                                     className="schbtnGnb"
                                     title="Open search"
                                     onClick={goSearch}
                                 />
-
                                 {/* 입력창 */}
                                 <input
                                     id="schinGnb"
@@ -191,67 +164,54 @@ const Layout = () => {
                                     placeholder="Filter by Keyword"
                                     onKeyUp={enterKey}
                                 />
-
                             </div>
-
-                            {/* 검색 기능 링크 - 클릭 시 검색창 보이기! */}
+                            {/* 검색기능링크 - 클릭시 검색창보이기! */}
                             <a href="#" onClick={showSearch}>
                                 <FontAwesomeIcon icon={faSearch} />
                             </a>
                         </li>
-
-                        {/* 회원가입, 로그인은 로그이 아닌 상태일 때만 */}
-                        {logSts === null &&
-
-                            <>
-                                {/* 회원가입 */}
-                                <li>
-                                    <Link to="/mem">Join Us</Link>
-                                </li>
-
-                                {/* 로그인 */}
-                                <li>
-                                    <Link to="/login">LOGIN</Link>
-                                </li>
-                            </>
+                        {
+                            /* 회원가입,로그인은 로그인아닌 상태일때만 */
+                            logSts === null && (
+                                <>
+                                    <li onClick={chgMenu}>
+                                        <Link to="/mem">Join Us</Link>
+                                    </li>
+                                    <li onClick={chgMenu}>
+                                        <Link to="/login">LOGIN</Link>
+                                    </li>
+                                </>
+                            )
                         }
 
-                        {/* 로그아웃은 로그인 상태일 때만 */}
                         {
-                            logSts !== null &&
-
-                            <>
-                                {/* 로그아웃 */}
-                                <li>
+                            /* 로그아웃버튼은 로인인상태일때만 */
+                            logSts !== null && (
+                                <li onClick={chgMenu}>
                                     <a href="#" onClick={logout}>
                                         LOGOUT
                                     </a>
                                 </li>
-                            </>
+                            )
                         }
-                        
                     </ul>
+                    {/* 햄버거버튼 */}
+                    <button className="hambtn" onClick={chgMenu}></button>
                 </nav>
             </header>
-
-            {/* 2. 메인 영역 */}
+            {/* 2. 메인영역 */}
             <main className="cont">
-
-                {/* 출력 파트: 각 페이지의 컴포넌트 출력 */}
+                {/* 출력파트 : 각 페이지의 컴포넌트가 출력됨 */}
                 <Outlet />
-                
             </main>
-
-            {/* 3. 하단 영역 */}
+            {/* 3.하단영역 */}
             <footer className="info">
-
                 <ul>
                     <li>
                         <Logo gb="bottom" tt={callMe} />
                     </li>
                     <li>
                         <ol className="bmenu">
-                            
                             {bmenu.map((v, i) => (
                                 <li key={i}>
                                     <a href={v.link} target="_blank">
@@ -259,20 +219,14 @@ const Layout = () => {
                                     </a>
                                 </li>
                             ))}
-
                         </ol>
                     </li>
-
                     <li>© & ™ DC. ALL RIGHTS RESERVED</li>
-                    
                 </ul>
             </footer>
-    
         </>
-        
-    ); // return //
-    
-}; // Layout 컴포넌트 //
+    );
+}; ////////// Layout 컴포넌트 ///////
 
-// 컴포넌트 내보내기
+// 내보내기
 export default Layout;
--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/plugin/SwiperCat.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/plugin/SwiperCat.js
@@ -1,39 +1,34 @@
 import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-
-/* 제이쿼리 넣기 */
-import $ from 'jquery';
+/* 제이쿼리넣기 */
+import $ from "jquery";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
 
-// CSS 가져오기
 import "./swipercat.css";
 
 // import required modules
 import { Navigation } from "swiper";
-
 // 데이터 가져오기
 import cat_data from "../data/cat";
 import { Link } from "react-router-dom";
 
-export default function SwiperCat(props){
-
+export default function SwiperCat(props) {
     // 데이터 셋팅
     const sdt = cat_data;
     console.log(sdt);
-    
+
     return (
         <>
             <Swiper
-                // 한 화면당 개수를 Hook 변수 사용
                 // slidesPerView={2}
                 spaceBetween={20}
                 navigation={true}
                 modules={[Navigation]}
-                // 스와이퍼 사이즈별 슬라이드 수 변경
+                // 스와이퍼 사이즈별 슬라이드수 변경!
                 breakpoints={{
                     200: {
                         slidesPerView: 3,
@@ -48,53 +43,42 @@ export default function SwiperCat(props){
                         slidesPerView: 7,
                     },
                 }}
-                className="mySwiper"
-            >
-
-                {sdt.map((v,i) =>
-                /* 저스티스리그 캐릭터는 idx 1~7 */
-                Number(v.idx) <= 7 && (
-                    <SwiperSlide key={i}>
-
-                        {/* /det 라우터 컴포넌트 페이지 호출 시 state 속성값으로 객체를 보내어 값을 전달함 */}
-
-                        {/* 도착 페이지인 Detail.js 컴포넌트에 페이지를 나타내야 할 데이터 항목을 데이터 속성명과 같은 이름으로 셋팅하여 라우터 전달 state객체에 담아서 보냄*/}
-
-                        {/* selcat은 캐릭터 이름 전달 속성명 */}
-                        {/* 값은 v.cname으로 캐릭터명을 보내줌 */}
-
-                        {/*
-                            cname 캐릭터 이름
-                            cdesc 캐릭터 설명
-                            facts 캐릭터 명세
-                        */}
-                        
-                        <Link to="/det" 
-                        state={
-                            {
-                                cname:v.cname,
-                                cdesc:v.cdesc,
-                                facts:v.facts
-                            }
-                        }>
-                            <section className="swinbx">
-
-                                {/* 캐릭터 이미지 영역 */}
-                                <div className="catimg">
-                                    <img src={v.tmsrc} alt={v.cname} />
-                                </div>
-                                
-                                {/* 캐릭터 타이틀 영역 */}
-                                <div className="cattit">
-                                    <h3>{v.cname}</h3>
-                                </div>
-
-                            </section>
-                        </Link>
-                    </SwiperSlide>
-
-                ))}
-
+                className="mySwiper2">
+                {sdt.map(
+                    (v, i) =>
+                        /* 저스티스리그 캐릭터는 idx 1~7 */
+                        Number(v.idx) <= 7 && (
+                            <SwiperSlide key={i}>
+                                {/* "/det" 라우터 컴포넌트 페이지 호출시
+                            state속성값으로 객체를 보내어 값을 전달함!
+                            도착페이지인 Detail.js 컴포넌트에
+                            페이지 나타내야할 데이터 항목을 
+                            데이터 속성명과 같은 이름으로 셋팅하여
+                            라우터 전달 state객체에 담아서 보낸다!
+                            cname은 캐릭터이름
+                            cdesc는 캐릭터설명
+                            facts는 캐릭터명세 정보임! */}
+                                <Link
+                                    to="/det"
+                                    state={{
+                                        cname: v.cname,
+                                        cdesc: v.cdesc,
+                                        facts: v.facts,
+                                    }}>
+                                    <section className="swinbx2">
+                                        {/* 캐릭터이미지영역 */}
+                                        <div className="catimg2">
+                                            <img src={v.tmsrc} alt={v.cname} />
+                                        </div>
+                                        {/* 동영상타이틀영역 */}
+                                        <div className="cattit2">
+                                            <h3>{v.cname}</h3>
+                                        </div>
+                                    </section>
+                                </Link>
+                            </SwiperSlide>
+                        )
+                )}
             </Swiper>
         </>
     );
